709e568541d7a529498dbd73deb934ec684805bc
Jungmin <miniharu.zip@gmail.com>
2024-10-03 14:13:02 +0900

    feat:Next.js 14 버전 및 ESLint, Prettier, Tailwind CSS 설정
    - Next.js 14 버전 설치
    - ESLint 및 Prettier 설정 추가
    - Tailwind CSS 스타일링 설정 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,42 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: "#FFFFFF",
+        black: "#000000",
+
+        // BLUE SCALE
+        blue: {
+          50: "#EFF6FF",
+          100: "#DBEAFE",
+          200: "#BFDBFE",
+          300: "#93C5FD",
+          400: "#60A5FA",
+          500: "#3B82F6",
+          600: "#2563EB",
+          700: "#1D4ED8",
+          800: "#1E40AF",
+          900: "#1E3A8A",
+          950: "#172554",
+        },
+
+        // SLATE SCALE
+        slate: {
+          50: "#F8FAFC",
+          100: "#F1F5F9",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          400: "#94A3B8",
+          500: "#64748B",
+          600: "#475569",
+          700: "#334155",
+          800: "#1E293B",
+          900: "#0F172A",
+          950: "#020617",
+        },
+
+        red: {
+          50: "#FF7777",
+        },
       },
     },
   },
