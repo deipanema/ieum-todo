0cba640d457168e4a145423da7acc0cf284097a4
Jungmin <miniharu.zip@gmail.com>
2024-10-03 15:04:32 +0900

    feat: 회원가입 페이지 개발
    - 회원가입 페이지 레이아웃 구성 및 스타일링
    - 회원가입 폼 개발 (닉네임, 아이디, 비밀번호, 비밀번호 확인)
    - 입력 필드 및 제출 버튼 컴포넌트 개발
    - 로그인 링크 추가
    - 유효성 검사

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import SignupFormContainer from "../components/SignupFormContainer";
+
+export default function SignupPage() {
+  return (
+    <section className="bg-gray-2 text-gray-15 mb-8 flex flex-col justify-center pt-8 md:flex-row">
+      <SignupFormContainer />
+    </section>
+  );
+}
