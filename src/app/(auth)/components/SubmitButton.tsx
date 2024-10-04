efc16b0f4a4dbf7a223c6d4b2249044abac04db1
Jungmin <miniharu.zip@gmail.com>
2024-10-04 09:24:47 +0900

    refactor: 버튼에 로딩스피너 추가

--- src/app/(auth)/components/SubmitButton.tsx
+++ src/app/(auth)/components/SubmitButton.tsx
@@ -1,3 +1,5 @@
+import LoadingSpinner from "@/app/components/LoadingSpinner";
+
 interface SubmitButtonProps {
   isSubmitting: boolean;
   text: string;
@@ -8,9 +10,9 @@ export default function SubmitButton({ isSubmitting, text }: SubmitButtonProps) 
     <button
       type="submit"
       disabled={isSubmitting}
-      className={`mb-4 mt-[15px] flex h-[30px] items-center justify-center rounded-sm border py-[6px] text-xs text-white sm:mb-6 sm:mt-4 sm:h-11 sm:text-base md:py-[10px] ${isSubmitting ? "border-slate-100 bg-slate-50" : "border-blue-500 bg-blue-500"}`}
+      className="mb-4 mt-[15px] flex h-[30px] items-center justify-center rounded-sm border bg-blue-400 py-[6px] text-xs text-white hover:bg-blue-500 sm:mb-6 sm:mt-4 sm:h-11 sm:text-base md:py-[10px]"
     >
-      {text}
+      {`${isSubmitting ? <LoadingSpinner /> : "회원가입하기"}`}
     </button>
   );
 }
---
+++ src/app/components/LoadingSpinner.tsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+export default function LoadingSpinner() {
+  return (
+    <motion.div
+      data-testid="spinner"
+      animate={{ rotate: 360 }}
+      transition={{
+        repeat: Infinity,
+        duration: 1,
+        ease: "linear",
+      }}
+    >
+      <Image src="/spinner.png" alt="Loading..." width={17} height={17} />
+    </motion.div>
+  );
+}
