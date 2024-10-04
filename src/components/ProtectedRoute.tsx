7fc068faeee659f8ec04576b41d1f258f30a130d
Jungmin <miniharu.zip@gmail.com>
2024-10-05 08:17:17 +0900

    refactor: 미들웨어 수정 및 대시보드 ProtectedRoute 수정
    - middleware로 보호: Next.js의 middleware를 활용하여 /dashboard/* 경로에 대해 인증 체크를 합니다.
    - ProtectedRoute로 페이지 보호: 각 대시보드 페이지에서 ProtectedRoute 컴포넌트를 사용하여 인증된 사용자만 페이지에 접근할 수 있도록 설정합니다.
     - 사용자가 대시보드의 어떤 버튼을 클릭하더라도 로그인 상태가 유지되고, 인증되지 않은 사용자는 로그인 페이지로 리디렉션됩니다.

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,27 +4,19 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { useAuthStore } from "@/store/AuthStore";
-import { checkAuth } from "@/utils/authUtils";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
   useEffect(() => {
-    const verifyAuth = async () => {
-      if (!isAuthenticated) {
-        const authCheck = await checkAuth();
-        if (!authCheck) {
-          router.push("/login");
-        }
-      }
-    };
-
-    verifyAuth();
+    if (!isAuthenticated) {
+      router.push("/login"); // 인증되지 않으면 로그인 페이지로 리디렉션
+    }
   }, [isAuthenticated, router]);
 
   if (!isAuthenticated) {
-    return null;
+    return <div>Loading...</div>; // 인증 확인 중에 로딩 표시
   }
 
   return <>{children}</>;
