19cbe7dd34ab8d754086ce3db3a4d9e4f56413ea
Jungmin <miniharu.zip@gmail.com>
2024-10-04 09:27:18 +0900

    WIP

--- src/app/(auth)/components/SubmitButton.tsx
+++ src/app/(auth)/components/SubmitButton.tsx
@@ -12,7 +12,7 @@ export default function SubmitButton({ isSubmitting, text }: SubmitButtonProps) 
       disabled={isSubmitting}
       className="mb-4 mt-[15px] flex h-[30px] items-center justify-center rounded-sm border bg-blue-400 py-[6px] text-xs text-white hover:bg-blue-500 sm:mb-6 sm:mt-4 sm:h-11 sm:text-base md:py-[10px]"
     >
-      {`${isSubmitting ? <LoadingSpinner /> : "회원가입하기"}`}
+      {`${isSubmitting ? <LoadingSpinner /> : text}`}
     </button>
   );
 }
