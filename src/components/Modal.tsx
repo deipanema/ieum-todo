0a0a881c1dfa846a07185b415c5255ad9a8714ae
Jungmin <miniharu.zip@gmail.com>
2024-10-07 00:01:02 +0900

    fix: 오버레이 순서대로 닫기

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -19,15 +19,15 @@ export default function Modal({ children, type }: ModalProps) {
     closeModal(type);
   };
 
-  const handleOverlayClick = (e: React.MouseEvent) => {
-    if (type === "parent" && e.target === e.currentTarget) {
-      handleClose();
-    }
-  };
+  //   const handleOverlayClick = (e: React.MouseEvent) => {
+  //     if (type === "parent" && e.target === e.currentTarget) {
+
+  //     }
+  //   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center" onClick={handleOverlayClick}>
-      <div className="fixed inset-0 bg-black bg-opacity-50" />
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <div className="fixed inset-0 bg-black bg-opacity-50" onClick={handleClose} />
       <div className="relative z-50 w-full max-w-lg rounded-xl bg-white p-6">
         {children}
         <button onClick={handleClose} className="absolute right-5 top-6 text-2xl text-slate-500 hover:text-slate-800">
