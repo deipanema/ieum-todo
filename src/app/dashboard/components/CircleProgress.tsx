209c706516dccbdcadd0d4ba91ce07da2b4d40f1
Jungmin <miniharu.zip@gmail.com>
2024-10-10 11:52:17 +0900

    Circle progress 비율 채우기 및 로그아웃 호버 애니매이션

--- src/app/dashboard/components/CircleProgress.tsx
+++ src/app/dashboard/components/CircleProgress.tsx
@@ -18,7 +18,7 @@ export default function CircleProcess() {
       <svg width="166" height="166" viewBox="0 0 166 166" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="graph">
           <circle id="Ellipse 215" cx="83" cy="83" r="67" stroke="#000000" strokeWidth="32" />
-          <circle className="progress-ring__circle" cx="83" cy="83" r="67" stroke="#F8FAFC" strokeWidth="32" />
+          <circle className="progress-ring-circle" cx="83" cy="83" r="67" stroke="#F8FAFC" strokeWidth="32" />
         </g>
       </svg>
     </div>
