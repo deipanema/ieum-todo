92e7de0df40d8b4ba5fe09667986a9102f663efc
Jungmin <miniharu.zip@gmail.com>
2024-10-12 22:55:19 +0900

    feat: 노트 작성 페이지
    - 할 일 별 노트 작성: 할 일에 대한 상세한 노트를 작성할 수 있습니다.
        - 노트 제목: 최대 30자 이하 유효성 검사를 실시합니다.
        - 본문: 글자 수 세는 기능을 제공합니다.
        - 링크: 참고하여 노트를 작성할 콘텐츠의 링크를 추가할 수 있습니다.
    - 5분에 한번 자동 임시 저장되거나, 임시 저장 버튼 클릭을 통해 저장이 가능합니다.
    - 임시 저장된 노트 불러오기 버튼: 로컬 스토리지에 데이터가 있으면, 할일 별 노트 작성 페이지를 처음 들어갔을 때, 토스트가 떠서 불러오기 할 수 있어야 합니다.

---
+++ src/components/EditGoalTitleModal.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { PatchGoal } from "@/api/goalAPI";
+
+export type GoalType = {
+  id: number;
+  teamId: string;
+  title: string;
+  userId: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type EditGoalTitleModalProps = {
+  closeEditTitle: () => void;
+  goals: GoalType;
+};
+
+export default function EditGoalTitleModal({ closeEditTitle, goals }: EditGoalTitleModalProps) {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await PatchGoal(goals.id, title);
+      closeEditTitle();
+    } catch (error) {
+      console.error("Error updating goal:", error);
+    }
+  };
+
+  useEffect(() => {
+    setTitle(goals.title);
+  }, [goals]);
+
+  return (
+    <form className="flex select-none flex-col gap-6" onSubmit={handleSubmit}>
+      <div>
+        <h3 className="mb-3 font-semibold">제목</h3>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full rounded-xl bg-[#F8FAFC] px-6 py-3 focus:outline-none"
+          placeholder="목표의 새 타이틀"
+          autoFocus
+        />
+      </div>
+      <div>
+        <button
+          disabled={title.length === 0}
+          className="w-full rounded-xl bg-blue-400 py-3 text-base text-white hover:bg-blue-500 disabled:bg-blue-200"
+        >
+          확인
+        </button>
+      </div>
+    </form>
+  );
+}
