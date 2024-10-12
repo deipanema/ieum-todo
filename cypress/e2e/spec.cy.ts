7234bd1d2ff4c48ae08f11515fc33309ac6beb89
Jungmin <miniharu.zip@gmail.com>
2024-10-12 11:06:20 +0900

    chore: cypress stting (2)
    - cy:open
    - cy:run
    - npm-run-all library install
    - wait-on library install

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,9 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("로그인 페이지 테스트", () => {
+  it.only("로그인 성공.", () => {
+    cy.visit("http://localhost:3000");
+    cy.contains("로그인");
+  });
+  it("로그인 실패.", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
