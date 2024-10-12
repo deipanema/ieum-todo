f17a146b0479710fc814e1a9a4a3e38c68ec916a
Jungmin <miniharu.zip@gmail.com>
2024-10-12 13:17:36 +0900

    test: cypress 로그인 테스트
    - 로그인 성공 시 테스트 실행
    - 로그인 실패 시 테스트 실행

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,21 @@
 describe("로그인 페이지 테스트", () => {
-  it.only("로그인 성공.", () => {
-    cy.visit("http://localhost:3000");
-    cy.contains("로그인");
+  it("로그인 성공.", () => {
+    cy.intercept("/dashboard*").as("dashboard");
+
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('input[placeholder="이메일"]').type("qq@naver.com");
+    cy.get('input[placeholder="비밀번호"]').type("012345678");
+    cy.contains("로그인").click();
+    cy.wait("@dashboard").then((_) => {
+      cy.contains("대시보드");
+    });
   });
-  it("로그인 실패.", () => {
-    cy.visit("https://example.cypress.io");
+  it.only("로그인 실패.", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('input[placeholder="이메일"]').type("wrong@naver.com");
+    cy.get('input[placeholder="비밀번호"]').type("wrongnumber");
+    cy.contains("로그인").click();
   });
 });
