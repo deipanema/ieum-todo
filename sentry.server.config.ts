df13be66b56ab130928c6f37e76d4026c28d3b30
Jungmin <miniharu.zip@gmail.com>
2024-10-14 11:10:03 +0900

    feat: github actions 및 Sentry 설정 및 에러 추적 코드 추가
    * sentry.client.config.ts, sentry.edge.config.ts, sentry.server.config.ts: Sentry 설정 완료
    * src/app/global-error.tsx: 글로벌 에러 발생 시 Sentry에 보고하도록 수정
    * src/app/sentry-example-page/page.tsx: 예시 페이지에 Sentry 이벤트 발생 코드 추가
    
    production 환경에서 발생하는 에러를 추적하고 분석하기 위해 Sentry를 설정했습니다.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://862e54ea28fcbd49ac1dd43dbd5de10d@o4507882883973120.ingest.us.sentry.io/4508118629613568",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
