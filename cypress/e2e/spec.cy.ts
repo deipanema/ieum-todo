37da5d02fd332b6d9804c7c770f9502db58dfdaa
Jungmin <miniharu.zip@gmail.com>
2024-10-14 21:45:23 +0900

    WIP

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -12,17 +12,17 @@ describe("로그인 페이지 테스트", () => {
     });
   });
 
-  it("로그인 실패.", () => {
-    cy.intercept("/login").as("login");
+  // it("로그인 실패.", () => {
+  //   cy.intercept("/login").as("login");
 
-    cy.visit("http://localhost:3000/login");
+  //   cy.visit("http://localhost:3000/login");
 
-    cy.get('input[placeholder="이메일"]').type("wrong@naver.com");
-    cy.get('input[placeholder="비밀번호"]').type("wrongnumber");
-    cy.contains("로그인").click();
+  //   cy.get('input[placeholder="이메일"]').type("wrong@naver.com");
+  //   cy.get('input[placeholder="비밀번호"]').type("wrongnumber");
+  //   cy.contains("로그인").click();
 
-    cy.wait("@login").then(() => {
-      cy.contains("로그인에 실패했습니다. 다시 시도해 주세요.").should("be.visible");
-    });
-  });
+  //   cy.wait("@login").then(() => {
+  //     cy.contains("로그인에 실패했습니다. 다시 시도해 주세요.").should("be.visible");
+  //   });
+  // });
 });
