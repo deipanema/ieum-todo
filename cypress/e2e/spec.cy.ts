db4c6398696f537075c30ea37a07c231dab89d8b
Jungmin <miniharu.zip@gmail.com>
2024-10-14 21:42:11 +0900

    fix: Cypress 로그인 실패 수정

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -7,18 +7,22 @@ describe("로그인 페이지 테스트", () => {
     cy.get('input[placeholder="이메일"]').type("qq@naver.com");
     cy.get('input[placeholder="비밀번호"]').type("012345678");
     cy.contains("로그인").click();
-    cy.wait("@dashboard").then((_) => {
+    cy.wait("@dashboard").then(() => {
       cy.contains("대시보드");
     });
   });
-  it.only("로그인 실패.", () => {
+
+  it("로그인 실패.", () => {
+    cy.intercept("/login").as("login");
+
     cy.visit("http://localhost:3000/login");
 
     cy.get('input[placeholder="이메일"]').type("wrong@naver.com");
     cy.get('input[placeholder="비밀번호"]').type("wrongnumber");
     cy.contains("로그인").click();
 
-    // 실패 시 에러 메시지 확인
-    cy.contains("로그인에 실패했습니다. 다시 시도해 주세요.");
+    cy.wait("@login").then(() => {
+      cy.contains("로그인에 실패했습니다. 다시 시도해 주세요.").should("be.visible");
+    });
   });
 });
