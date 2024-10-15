98dee9e64fd96fcea092b0e1a6d05a44974c3eda
Jungmin <miniharu.zip@gmail.com>
2024-10-15 13:51:52 +0900

    test: login 테스트

--- src/app/(auth)/components/SignupForm.test.tsx
+++ src/app/(auth)/components/SignupForm.test.tsx
@@ -3,6 +3,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { toast } from "react-toastify";
+import { vi } from "vitest";
 
 import { postUser } from "@/api/authAPI";
 
